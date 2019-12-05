fa3287c0219b87bfc59631ba5eb5399ee32b6ff8
Hochul <hchlhwang@kist.re.kr>
2019-12-05 13:39:41 +0900

    [no error] SYN X-SUBJ TESTED by modifying inference.py & train_and_evaluate.sh

--- src/script/train_and_evaluate.sh
+++ src/script/train_and_evaluate.sh
@@ -14,11 +14,11 @@ ntu=$2
 
 # Training the CNN-backbone
 #python src/main.py --dir $dir --root $ntu -b 10 -j 4 --epochs 10 --time 8 --pose-predictions
-CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 10 -j 8 --epochs 10 --time 8 --pose-predictions
+CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 64 -j 8 --epochs 10 --time 8 --pose-predictions
 
 # Training the Glimpse Clouds
 #python src/main.py --dir $dir --root $ntu -b 32 -j 4 --epochs 10 --time 8 --glimpse-clouds --pose-predictions
-CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 32 -j 8 --epochs 10 --time 8 --glimpse-clouds --pose-predictions
+CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 64 -j 8 --epochs 10 --time 8 --glimpse-clouds --pose-predictions
 # Evaluating on the test set
 #python src/main.py --dir $dir --root $ntu -b 32 -j 4 --time 8 --glimpse-clouds --pose-predictions -e
-CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 32 -j 8 --time 8 --glimpse-clouds --pose-predictions -e
+CUDA_VISIBLE_DEVICES=0,1 python src/main.py --dir $dir --root $ntu -b 64 -j 8 --time 8 --glimpse-clouds --pose-predictions -e
